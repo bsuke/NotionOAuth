5d4bc979be8a3f04773ec87d77c2d2577955bf6d
cando <kandokando1115@gmai;.cpm>
2023-08-19 21:27:30 +0900

    Updated redirectUri directly in the code

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const express = require('express');
-const fetch = require('node-fetch');  // 追加: Notion APIへのリクエストに使用
+const fetch = require('node-fetch');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
-// 基本的なルートハンドラの追加
 app.get('/', (req, res) => {
     res.send('Hello, this is my Notion OAuth server!');
 });
@@ -17,11 +16,11 @@ app.get('/oauth-callback', async (req, res) => {
     // ここで認証コードをログとして出力
     console.log("Received authorization code:", code);
     
-    // 一時的な認証コードを使用してaccess_tokenを取得
     const clientId = process.env.OAUTH_CLIENT_ID;
     const clientSecret = process.env.OAUTH_CLIENT_SECRET;
-    const redirectUri = process.env.OAUTH_REDIRECT_URI;
+    const redirectUri = "https://notion-o-auth.vercel.app/oauth-callback";  // 直接URLを書き込み
     const encoded = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
+    
     const response = await fetch("https://api.notion.com/v1/oauth/token", {
         method: "POST",
         headers: {
@@ -37,10 +36,8 @@ app.get('/oauth-callback', async (req, res) => {
     });
     const data = await response.json();
 
-    // Notion APIからのレスポンスをログに出力
-    console.log(data);  // この行を追加
+    console.log(data);
     
-    // access_tokenとworkspace_idをWebページに表示
     res.render('display', { accessToken: data.access_token, databaseId: data.workspace_id });
 });
 
